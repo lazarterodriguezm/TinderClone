730f7993781e9b6219fa672dfb68576622bb1bcd
lazarterodriguezm <lazarterodriguezm@gmail.com>
2023-01-29 22:56:27 +0100

    Card rotation behaviour

--- App.tsx
+++ App.tsx
@@ -1,13 +1,63 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import Card from './src/components/TinderCard';
 import users from './assets/data/users';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useDerivedValue,
+  useAnimatedGestureHandler,
+  interpolate,
+} from 'react-native-reanimated';
+import {GestureHandlerRootView, PanGestureHandler} from 'react-native-gesture-handler';
+
+const ROTATION = 60;
 
 const App = () => {
+  const {width: screenWidth} = useWindowDimensions();
+
+  const hiddenTranslateX = 2 * screenWidth;
+
+  const translateX = useSharedValue(0);
+  const rotate = useDerivedValue(
+    () =>
+      interpolate(translateX.value, [0, hiddenTranslateX], [0, ROTATION]) +
+      'deg',
+  );
+
+  const cardStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        rotate: rotate.value,
+      },
+    ],
+  }));
+
+  const gestureHandler = useAnimatedGestureHandler({
+    onStart: (_, context) => {
+      context.startX = translateX.value;
+    },
+    onActive: (event, context) => {
+      translateX.value = context.startX + event.translationX;
+    },
+    onEnd: () => {
+      console.warn('Touch ended');
+    },
+  });
+
   return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
     <View style={styles.pageContainer}>
-      <Card user={users[2]} />
+      <PanGestureHandler onGestureEvent={gestureHandler}>
+        <Animated.View style={[styles.animatedCard, cardStyle]}>
+        <Card user={users[2]} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
+    </GestureHandlerRootView>
   );
 };
 
@@ -18,6 +68,12 @@ const styles = StyleSheet.create(
       alignItems: 'center',
       flex: 1
     },
+    animatedCard: {
+      width: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      rotate: '60deg',
+    }
   },
 );
 
